9dc6500edd2c5594ea52edf3a8bf34195244593c
LironJit <liron@jit.io>
2023-04-13 13:28:50 +0300

    handling exceptions and anomalies, added a .txt summary, changed filenames, bug fixes, some syntactic revisions

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import re
 import json
 import csv
 import string
+import os
+from datetime import datetime
 from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill
 
@@ -11,20 +13,49 @@ def main():
     # Define PATH of repo directories that the tool will scan
     directories = [
         "/Users/lironbiam/Documents/GoatRepositories/Kotlin/AndroGoat",
-        "/Users/lironbiam/Documents/GoatRepositories/habitica-android",
+        "/Users/lironbiam/Documents/GoatRepositories/Kotlin/habitica-android",
         "/Users/lironbiam/Documents/GoatRepositories/Kotlin/Goatlin",
-        "/Users/lironbiam/Documents/GoatRepositories/Kotlin/NotyKT"
+        "/Users/lironbiam/Documents/GoatRepositories/Kotlin/NotyKT",
+        "/Users/lironbiam/Documents/GoatRepositories/Kotlin/ivy-wallet",
+        "/Users/lironbiam/Documents/GoatRepositories/Kotlin/ytvanced"
     ]
 
-    # Tool definitions, see Readme.md for more information
-    output_file = 'findings.json'  # Don't have to change this
+    # OPTIONAL DEFINITIONS (if unchanged, program will still run successfully)
+    output_file = 'findings.json'  # Don't have to change this but make sure it matches the tool_cmd output file
+    tool_version = "v2.8.0"  # define tool version. if left 'undefined', program attempts to auto generate. if fails - version will not appear in reports.
+
+    # MANDATORY DEFINITIONS 
     tool_cmd = ["horusec", "start", "-p=.", "-o=json", f"-O=./{output_file}", "-s=LOW"]  # CLI command for tool scan
     headers = ['severity', 'language', 'confidence', 'code', 'details']  # Headers for the CSV output file, matches the jq command's headers that we're keeping when filtering
-    jq_cmd = ['jq', '[.analysisVulnerabilities[].vulnerabilities | {severity, language, confidence, code, details}]']
-    # jq_cmd = ['jq', f'[.analysisVulnerabilities[].vulnerabilities | {{ {", ".join([f"{header}: .{header}" for header in headers])} }}]']  # Another method, more dynamic but less readable
+    jq_cmd = ['jq', '[.analysisVulnerabilities[].vulnerabilities | {severity, language, confidence, code, details}]']    
+    
+    #
+    # DO NOT CHANGE ANYTHING BELOW THIS COMMENT
+    #
+
+    # Auto-defined variables
+    tool_name = tool_cmd[0]
+    # if tool version is undefined, try to get it automatically. if '--version' does not work, don't mention the version.
+    if tool_version == 'undefined':
+        try:
+            tool_version = f'{subprocess.check_output([tool_name, "--version"]).decode().strip()}'
+            output_file_name = f"{tool_name}-{tool_version}"  # use the automatically generated version
+        except subprocess.CalledProcessError:
+            output_file_name = f"{tool_name}"
+    else:
+        output_file_name = f"{tool_name}-{tool_version}"  # use the hard-coded version
+
 
     # Run CLI commands for each one of the directories defined above
     for directory in directories:
+
+        # clean up (delete old findings files) from a previous scan to prevent false positives and only maintain new data
+        cleanup_files = [output_file, "parsed.json", "parsed.csv"]
+        for file_name in cleanup_files:
+            file_path = os.path.join(directory, file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
         # Extract project name from directory path
         project_name = re.search(r'/([^/]+)/?$', directory).group(1)
         print(f'Current working directory: {project_name}')
@@ -40,32 +71,36 @@ def main():
             subprocess.run(jq_cmd, cwd=directory, stdout=f, input=open(f"{directory}/{output_file}").read().encode())
         
         # Convert parsed JSON files to CSV files
-        with open(f"{directory}/parsed.json") as f:
-            data = json.load(f)
-        with open(f"{directory}/parsed.csv", "w", newline='') as f:
-            writer = csv.writer(f)
-            headers_row = ['Repository'] + headers
-            writer.writerow(headers_row)
-            for item in data:
-                # Handle non-printable ASCII characters in conversion from .json to .csv
-                data_row = [project_name] + ["".join(filter(lambda x: x in string.printable, str(item[header]))) for header in headers]
-                writer.writerow(data_row)
+        if os.path.exists(f'{directory}/parsed.json') and os.path.getsize(f'{directory}/parsed.json') > 0:
+            with open(f"{directory}/parsed.json") as f:
+                data = json.load(f)
+            with open(f"{directory}/parsed.csv", "w", newline='') as f:
+                writer = csv.writer(f)
+                headers_row = ['Repository'] + headers
+                writer.writerow(headers_row)
+                for item in data:
+                    # Handle non-printable ASCII characters in conversion from .json to .csv
+                    data_row = [project_name] + ["".join(filter(lambda x: x in string.printable, str(item[header]))) for header in headers]
+                    writer.writerow(data_row)
+        else:
+            print(f'> {project_name} has no findings therefore no parsed.csv. Skipping..')
 
     # Merge findings from all directories into a single CSV file
-    with open('merged_findings.csv', 'w', newline='') as f:
+    with open(f'{output_file_name}.csv', 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['Repository'] + headers)
         for directory in directories:
             project_name = re.search(r'/([^/]+)/?$', directory).group(1)
-            with open(f"{directory}/parsed.csv") as subdir_csv:
-                subdir_reader = csv.reader(subdir_csv)
-                next(subdir_reader)  # skip header row
-                for row in subdir_reader:
-                    # Write all fields except the Repository field
-                    writer.writerow([project_name] + row[1:])
+            if os.path.isfile(f'{directory}/parsed.csv'):
+                with open(f"{directory}/parsed.csv") as subdir_csv:
+                    subdir_reader = csv.reader(subdir_csv)
+                    next(subdir_reader)  # skip header row
+                    for row in subdir_reader:
+                        # Write all fields except the Repository field
+                        writer.writerow([project_name] + row[1:])
     
     # convert .csv to .xlsx
-    with open('merged_findings.csv', 'r') as f:
+    with open(f'{output_file_name}.csv', 'r') as f:
         data = csv.reader(f)
         next(data)  # Skip header row
 
@@ -113,8 +148,31 @@ def main():
             ws.column_dimensions[column].width = adjusted_width
 
         # Save the Excel workbook
-        wb.save('findings.xlsx')
+        wb.save(f'{output_file_name}.xlsx')
+        print(f'> Findings saved into an Excel sheet named {output_file_name}.xlsx')
 
+    # summarize findings in .txt
+    with open(f'{output_file_name}.csv', 'r') as f:
+        data = csv.reader(f)
+        next(data)  # Skip header row
+        # Create a .txt file to summarize what the program did
+        repositories = [re.search(r'/([^/]+)/?$', directory).group(1) for directory in directories]
+        total_repositories_scanned = len(repositories)
+        timestamp = datetime.now().strftime("%d-%m-%Y")
+        lines_in_csv = sum(1 for _ in data)  # count instances in csv for the summarization .txt file later on
+
+        # Create .txt file and write the required information
+        with open(f'{output_file_name}.txt', 'w') as t:
+            t.write(f"{tool_name} - {tool_version}\n")
+            t.write(f"{timestamp}\n\n")
+            t.write("CLI COMMANDS USED:\n")
+            t.write(f"{' '.join(tool_cmd)}\n")
+            t.write(f"{' '.join(jq_cmd)}\n\n")
+            t.write(f"Total Repositories Scanned: {total_repositories_scanned}\n")
+            t.write("Repositories: " + ', '.join(repositories) + "\n")
+            t.write(f"Total Findings: {lines_in_csv}\n")
+            print(f'> Report Sumumarization saved into a Text file named {output_file_name}.txt')
 
 if __name__ == "__main__":
+    csv.field_size_limit(10485760) # 10 MB
     main()
